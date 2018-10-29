6ea8e3f6fe1f7537958b48927d2c88858ff0361c
Bryan <Bryan@10.5.0.104>
2018-10-29 11:22:17 -0500

    cccc

---
+++ Prueba2/src/prueba2/Yanzapanta_Bryan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba2;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Yanzapanta_Bryan {
+    
+    
+    
+    
+}
